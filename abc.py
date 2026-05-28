ca3961f4a6353c29b90994251eb1992d5e11d683
nkmulle-png <nkmulle@webmd.net>
2026-05-28 17:18:08 +0530

    Update abc.py

--- abc.py
+++ abc.py
@@ -1,9 +1,9 @@
 def calculate_discount(price, user_type):
     """Calculate discount based on user type."""
     if user_type == "premium":
-        discount = 0.2111
+        discount = 0.95
     elif user_type == "standard":
-        discount = 0.21
+        discount = 0.31
     else:
         discount = 0.05
 
